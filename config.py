29a14a3bd99825c5a668bfb24ecaa298abdeec63
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-06-13 11:28:21 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-# ---------------- CONFIGURACIÓN ----------------
 TOKEN = "8459092113:AAFFJ0b7H5gFzYjgYGk_g_57cI709dhVRhI"
 TU_CHAT_ID = "8688232119"
 API_TELEGRAM = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 CATEGORIAS = ["Perfume", "Crema", "Maquillaje", "Líquido"]
-LIMITE = 10  # Envía cada 10 productos
+LIMITE = 10
